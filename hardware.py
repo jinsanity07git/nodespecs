8c519a2d3eca039e1785ce1e8e7bcffec27a101f
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2024-04-11 20:33:54 -0400

    [feat] add GPU infomation

--- hardware.py
+++ hardware.py
@@ -115,7 +115,58 @@ def info_net():
     print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
     print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
 
+def check_gpu():
+    import sys
+    import subprocess
+    try:
+        import GPUtil
+        from tabulate import tabulate
+        return True
+    except ModuleNotFoundError as e:
+        # Extract the name of the missing module
+        missing_module = str(e).split("'")[1]
+        print(f"Attempting to install missing module: {missing_module}")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", missing_module])
+        return False
+
+def info_gpu():
+    gpuenv = False
+    while not gpuenv:
+        gpuenv = check_gpu()
+
+    if gpuenv:
+        import GPUtil
+        from tabulate import tabulate
+        print("="*40, "GPU Details", "="*40)
+        gpus = GPUtil.getGPUs()
+        list_gpus = []
+        for gpu in gpus:
+            # get the GPU id
+            gpu_id = gpu.id
+            # name of GPU
+            gpu_name = gpu.name
+            # get % percentage of GPU usage of that GPU
+            gpu_load = f"{gpu.load*100}%"
+            # get free memory in MB format
+            gpu_free_memory = f"{gpu.memoryFree}MB"
+            # get used memory
+            gpu_used_memory = f"{gpu.memoryUsed}MB"
+            # get total memory
+            gpu_total_memory = f"{gpu.memoryTotal}MB"
+            # get GPU temperature in Celsius
+            gpu_temperature = f"{gpu.temperature} °C"
+            gpu_uuid = gpu.uuid
+            list_gpus.append((
+                gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+                gpu_total_memory, gpu_temperature, gpu_uuid
+            ))
+
+        print(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                        "temperature", "uuid")))
+        
+
 if __name__ == "__main__":
     info_sys()
     info_cpu()
     info_mem()
+    info_gpu()
