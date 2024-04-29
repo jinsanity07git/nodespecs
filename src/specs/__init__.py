57cac1778cc1b3fa17e349def392cc37ec2c964b
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2024-04-29 14:11:32 -0400

    [doc] v0.0.6 use bench_cpu via command line

--- src/specs/__init__.py
+++ src/specs/__init__.py
@@ -1,6 +1,7 @@
 from .hardware import *
+from .benchmark import bench_cpu
 
 # Version of the node-specs package
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 
--- src/specs/cmd.py
+++ src/specs/cmd.py
@@ -4,6 +4,17 @@ keymap = {
         "Linux"   :  "df -H",
         "macOS"   :  "df -H",
         "windows" :  "Get-PSDrive -PSProvider 'FileSystem'"
-     }
+     },
+
+     "OSrelease" : {
+        "Linux"   :  "cat /etc/os-release",
+        "macOS"   :  "",
+        "windows" :  "systeminfo | findstr /B /C:'OS'"
+     },
+     "SysmArch" : {
+        "Linux"   :  "uname -m",
+        "macOS"   :  "",
+        "windows" :  "systeminfo | findstr /B /C:'System Type'"
+     },
 }
 
