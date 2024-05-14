314a04fb30e9703c4dd317032121bbdcfd9477c9
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2024-05-14 09:18:56 -0400

    [fix] add info_gpu to the lib

--- src/specs/__init__.py
+++ src/specs/__init__.py
@@ -1,7 +1,7 @@
 from .benchmark import (bench_cpu,info_plat)
 from .communicate.tcpsocket import (server,client)
 from .communicate.aboutme import whoish,derive_uid
-from .hardware import ensure_libs
+from .hardware import (ensure_libs,info_gpu)
 from .pyinfo import environment_check
 # Version of the node-specs package
 __version__ = "0.0.18"
